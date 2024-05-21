49e135d9a5ae6f30e6d3a6a442d325f7479efd0b
Arrigoni.20179 <Arrigoni.20179@LABI1-13W-S>
2024-05-21 09:44:54 +0200

    migliorie roulette

--- Form7.Designer.cs
+++ Form7.Designer.cs
@@ -30,28 +30,39 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form7));
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.Punta = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(280, 148);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label1.Location = new System.Drawing.Point(9, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(46, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
             // 
-            // button1
+            // Punta
             // 
-            this.button1.BackColor = System.Drawing.Color.Transparent;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button1.ForeColor = System.Drawing.Color.Transparent;
-            this.button1.Location = new System.Drawing.Point(397, 36);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(47, 75);
-            this.button1.TabIndex = 1;
-            this.button1.UseVisualStyleBackColor = false;
+            this.Punta.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.Punta.BackColor = System.Drawing.Color.Transparent;
+            this.Punta.ForeColor = System.Drawing.Color.Black;
+            this.Punta.Location = new System.Drawing.Point(12, 480);
+            this.Punta.Name = "Punta";
+            this.Punta.Size = new System.Drawing.Size(112, 42);
+            this.Punta.TabIndex = 1;
+            this.Punta.Text = "Punta";
+            this.Punta.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Location = new System.Drawing.Point(141, 492);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(213, 20);
+            this.textBox1.TabIndex = 2;
             // 
             // Form7
             // 
@@ -59,8 +70,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(1855, 1061);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1233, 534);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Punta);
             this.Controls.Add(this.label1);
             this.ForeColor = System.Drawing.Color.Transparent;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -75,6 +87,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Punta;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- Form7.cs
+++ Form7.cs
@@ -12,10 +12,12 @@ namespace Casino_Royale
         private static readonly HttpClient client = new HttpClient();
         private List<int> randomNumbers = new List<int>();
         private const string filePath = "randomNumbers.txt";
+        int[] array = new int[37];
 
         public Form7()
         {
             InitializeComponent();
+            CreateButtons();
             this.WindowState = FormWindowState.Maximized;
             this.BackgroundImageLayout = ImageLayout.Stretch; // Adatta l'immagine per riempire l'intero form
         }
@@ -43,7 +45,7 @@ namespace Casino_Royale
                 SaveRandomNumbersToFile();
 
                 // Aggiorna l'etichetta con il numero casuale
-                label1.Text = $"Numero casuale: {randomNumber}";
+                label1.Text = $"Numero uscito: {randomNumber}";
             }
             catch (Exception ex)
             {
@@ -77,6 +79,75 @@ namespace Casino_Royale
             catch (Exception ex)
             {
                 MessageBox.Show($"Errore durante il caricamento dei numeri casuali: {ex.Message}");
+            }
+        }
+
+        private void CreateButtons()
+        {
+            int buttonCount = 37;
+            int buttonWidth = 75;
+            int buttonHeight = 23;
+            int spacing = 10;
+
+            for (int i = 0; i < buttonCount; i++)
+            {
+                Button btn = new Button();
+                btn.Name = "button" + i;
+                btn.Text = "Button " + (i + 1);
+                btn.Width = buttonWidth;
+                btn.Height = buttonHeight;
+
+                // Posizione dei pulsanti
+                int x = (i % 5) * (buttonWidth + spacing);
+                int y = (i / 5) * (buttonHeight + spacing);
+
+                btn.Location = new System.Drawing.Point(x, y);
+                btn.Click += new EventHandler(Button_Click);
+
+                this.Controls.Add(btn);
+            }
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            // Metodo per resettare l'array impostando tutti gli elementi a 0
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = 38;
+            }
+
+            Button btn = sender as Button;
+            if (btn != null)
+            {
+
+                int buttonNumber = int.Parse(btn.Name.Substring("button".Length));
+
+                {
+                    // Esegui l'operazione in base al nome del pulsante
+                    switch (buttonNumber)
+                    {
+                        case 0:
+                            // Operazione per il pulsante 0
+                            MessageBox.Show("Operazione per il pulsante 0");
+                            array[0] = 0;
+                            break;
+                        case 1:
+                            // Operazione per il pulsante 1
+                            MessageBox.Show("Operazione per il pulsante 1");
+                            array[1] = 1;
+                            break;
+                        case 2:
+                            // Operazione per il pulsante 2
+                            MessageBox.Show("Operazione per il pulsante 2");
+                            array[2] = 2;
+                            break;
+                        // Aggiungi altri casi per i pulsanti rimanenti
+                        default:
+                            MessageBox.Show(btn.Name + " cliccato");
+                            array[buttonNumber] = buttonNumber;
+                            break;
+                    }
+                }
             }
         }
     }
