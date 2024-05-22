7e3f4456faf792f8f51f7cb3ab06fec870c7703c
FRANCOHAILIMITIDELLEVOLUTO <121171151+FRANCO8724@users.noreply.github.com>
2024-05-22 16:54:01 +0200

    Completamento form1 e quasi form2

--- Form2.cs
+++ Form2.cs
@@ -21,16 +21,42 @@ namespace Casino_Royale
             this.WindowState = FormWindowState.Maximized; //Adatta il form alla grandezza dello schermo
             this.MinimumSize = new Size(1100, 600); //Imposta la dimensione minima della finestra in modo da evitare che scompaiano bottoni o altri oggetti presenti nel form
             saldo = 0;//Assegno il valore saldo iniziale = a 0
+
+            label2.Visible = false;
+            label3.Visible = false;
         }
 
         //Quando l'utente preme il bottone gioca vengono eseguite tutte le operazioni contenute al suo interno
         private void button1_Click(object sender, EventArgs e)
         {
-            saldo = Convert.ToInt32(textBox1.Text);
+            int numericValue;
+            var a = textBox1.Text;
+            bool isNumber = int.TryParse(a, out numericValue);
 
-            this.Hide(); //Nasconde l'attuale form ovvero form3
-            Form3 home = new Form3(saldo); //Passo il conto dell'utente nel gioco del BlackJack
-            home.ShowDialog();//Mostra form5
+            if (isNumber == false)
+            {
+                label2.Visible = false;
+                textBox1.Text = "";
+                label3.Visible = true;
+            }
+            else
+            {
+                if (Convert.ToInt32(textBox1.Text) < 3001 && Convert.ToInt32(textBox1.Text) > 0)
+                {
+                    saldo = Convert.ToInt32(textBox1.Text);
+
+
+                    this.Hide(); //Nasconde l'attuale form ovvero form3
+                    Form3 home = new Form3(saldo); //Passo il conto dell'utente nel gioco del BlackJack
+                    home.ShowDialog();//Mostra form5
+                }
+                else
+                {
+                    label3.Visible = false;
+                    textBox1.Text = "";
+                    label2.Visible = true;
+                }
+            }
         }
 
 
---
+++ Funzio.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Casino_Royale
+{
+    internal class Funzio
+    {
+        public void Inserimento(string text,decimal saldo)
+        {
+            int numericValue;
+            var a = text;
+            bool isNumber = int.TryParse(a, out numericValue);
+
+            if (isNumber == false)
+            {
+                MessageBox.Show("Non puoi lasciare vuoto o inserire lettere");
+
+            }
+            else
+            {
+                if (Convert.ToInt32(text) < 3001 && Convert.ToInt32(text) > 0)
+                {
+                    MessageBox.Show(" La quantità inserità deve essere maggiore di 0 e inferiore o uguale a 3000");
+                }
+                else
+                {
+                    saldo = Convert.ToInt32(a);
+                }
+            }
+
+
+        }
+
+    }
+}
